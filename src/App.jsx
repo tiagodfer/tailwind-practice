f5110949865d25b674faf6a937175c94c1d9c3b7
Indra Santos <indrasantos@Indras-MacBook-Air.local>
2024-09-18 19:12:51 -0300

    added tailwind and react-icons

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { RiComputerFill } from "react-icons/ri";
+function App() {
+  return (
+    <div className="w-full flex flex-row justify-center items-center gap-2">
+      <h1>Me exclua e crie o seu componente aqui!</h1>
+      <RiComputerFill color="red" />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  important: "#root",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+};
